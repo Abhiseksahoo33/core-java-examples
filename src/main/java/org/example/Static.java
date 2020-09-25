27e777baa2ab044e3cc77311b35beebc582c5a81
abhisek <abhiseksahoo33@gmail.com>
2020-09-25 10:52:56 +0530

    static block

---
+++ src/main/java/org/example/Static.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class Static {
+
+    void m()
+    {
+        System.out.println("abhiek sahoo");
+    }
+    static {
+        System.out.println("hello");
+    }
+
+
+    public static void main(String[] args) {
+        Static obj =new Static();
+        obj.m();
+
+    }
+}
