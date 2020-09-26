42974e780fc51c8fe3a16d661a5d9d26a787d4e8
abhisek <abhiseksahoo33@gmail.com>
2020-09-26 22:59:39 +0530

    activity 4

--- src/main/java/org/example/variables/ParameterizedMethods.java
+++ src/main/java/org/example/variables/ParameterizedMethods.java
@@ -1,4 +1,19 @@
 package org.example.variables;
 
 public class ParameterizedMethods {
+    static int sum(int x,int y)
+    {
+        int z=x+y;
+        return z;
+
+    }
+    public static void main(String[] args) {
+int result=sum(10,20);
+        System.out.println("sum of two numbers is = "+result);
+    }
 }
+/**
+ * output
+ * sum of two numbers is = 30
+ */
+/
