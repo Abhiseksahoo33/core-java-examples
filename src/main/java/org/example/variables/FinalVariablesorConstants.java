0f36ee7ef0109735bb552ff870b7b3d8bb93caf1
abhisek <abhiseksahoo33@gmail.com>
2020-09-27 13:39:41 +0530

    final/ constant variable

--- src/main/java/org/example/variables/FinalVariablesorConstants.java
+++ src/main/java/org/example/variables/FinalVariablesorConstants.java
@@ -5,23 +5,24 @@ public class FinalVariablesorConstants {
     final  int y=30;
 
  static {
-     int x=34; // x will nt change  bcause x is static variable
+     int x=34; // x will nt change  bcause x is final  static variable
  }
     {
         int y=34;
-        System.out.println("value of y (local) = "+y); //  value of y will change because y is nonstatic
+        System.out.println("value of y (local) = "+y); //  value of y will change because y is local
     }
 
     public static void main(String[] args) {
         System.out.println("value of x = "+x);
         FinalVariablesorConstants o4=new FinalVariablesorConstants();
-        System.out.println("value of y (global and final/constant = "+o4.y);
+        System.out.println("value of y (global and final/constant = "+o4.y); // y is final variable
     }
 }
 
 /**
  *
  * output
- * value of x = 20
- * value of y = 34
+ value of x = 20
+ value of y (local) = 34
+ value of y (global and final/constant = 30
  */
