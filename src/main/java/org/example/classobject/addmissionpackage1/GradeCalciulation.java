67266e23eab2ab93a20f615c5fd707847109e39e
abhisek <abhiseksahoo33@gmail.com>
2020-10-01 19:13:55 +0530

    student sholarship calculation

---
+++ src/main/java/org/example/classobject/addmissionpackage1/GradeCalciulation.java
@@ -0,0 +1,27 @@
+package org.example.classobject.addmissionpackage1;
+
+
+class GradeCalculation {
+    String nameofstudent;
+    int passoutyear;
+    int markinpercentage;
+    int grade;
+    int scholarshipamount;
+
+
+    int scholarshipcalculation( ) {
+        if (markinpercentage>100)
+            System.out.println("invalid calculation of percentage");
+            System.out.println("markin percentage of "+nameofstudent+" is "+markinpercentage);
+              //  break ;
+        if (markinpercentage > 90 && markinpercentage <= 100)
+           return 6000;
+        if (markinpercentage >= 60 && markinpercentage < 90)
+            return 4000;
+        if (markinpercentage >= 50 && markinpercentage <60)
+            return 2000;
+        if (markinpercentage > 0 && markinpercentage < 50)
+            return 0;
+        return 0;
+    }
+}
---
+++ src/main/java/org/example/classobject/addmissionpackage1/GradeCalculationMain.java
@@ -0,0 +1,29 @@
+package org.example.classobject.addmissionpackage1;
+ class GradecalCulationMain
+{
+    public static void main(String[] args) {
+        GradeCalculation var1 =new GradeCalculation();
+        var1.nameofstudent="Abhisek";
+        var1.passoutyear=2022;
+        var1.markinpercentage=75;
+        System.out.println("scholarship amount for "+var1.nameofstudent+" is " +var1.scholarshipcalculation());
+
+        GradeCalculation var2 =new GradeCalculation();
+        var2.nameofstudent="prasad";
+        var2.passoutyear=2022;
+        var2.markinpercentage=45;
+        System.out.println("scholarship amount for "+var2.nameofstudent+" is " +var2.scholarshipcalculation());
+
+        GradeCalculation var3 =new GradeCalculation();
+        var3.nameofstudent="sambit";
+        var3.passoutyear=2022;
+        var3.markinpercentage=145;
+        System.out.println("scholarship amount for "+var3.nameofstudent+" is " +var3.scholarshipcalculation());
+
+
+        System.out.println(var3);
+        System.out.println(var2);
+        System.out.println(var1==var2);
+
+    }
+}
