2ccc81526cc0373b046a9bf4baa4905d9948beb4
abhisek <abhiseksahoo33@gmail.com>
2020-11-24 19:03:28 +0530

    reverse string using tocharArray

---
+++ src/main/java/org/example/array/ReverseString.java
@@ -0,0 +1,14 @@
+package org.example.array;
+
+class ReverseString {
+    public static void main(String[] args)
+    {
+        String s = "Abhisek sahoo";
+        
+        char[] arr = s.toCharArray();
+        int rev =arr.length;
+
+        for (int i = rev-1; i >= 0; i--)
+            System.out.print(arr[i]);//oohas kesihbA
+    }
+}
