4dd4e8b567beac260c055d27e912fa6d15ad1012
abhisek <abhiseksahoo33@gmail.com>
2020-11-24 18:39:50 +0530

    reverse the array

---
+++ src/main/java/org/example/StringClass/ReverseArray.java
@@ -0,0 +1,26 @@
+package org.example.StringClass;
+
+import java.util.Arrays;
+
+public class ReverseArray {
+
+    // this logic can be used to reverse any type of array
+    public static void main(String[] args) {
+        String s = "Abhisek  sahoo";
+        char[] ch = s.toCharArray();
+        int i = 0;
+        int j = ch.length - 1;
+
+        while (i < j) {
+            char temp = ch[i];
+            ch[i] = ch[j];
+            ch[j] = temp;
+
+            i = i + 1;
+            j = j - 1;
+        }
+
+        System.out.println(Arrays.toString(ch));  //[o, o, h, a, s,  ,  , k, e, s, i, h, b, A]
+
+    }
+}
