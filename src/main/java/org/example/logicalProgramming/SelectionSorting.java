37c64adffe4b9719217ee2a30712b48761e544c7
abhisek <abhiseksahoo33@gmail.com>
2020-12-05 21:32:14 +0530

    selection sorting

---
+++ src/main/java/org/example/logicalProgramming/SelectionSorting.java
@@ -0,0 +1,41 @@
+package org.example.logicalProgramming;
+
+import java.util.Arrays;
+
+public class SelectionSorting {
+
+   static void  selectionsorting(int[] arr)
+    {
+        System.out.println("Before sorting : "+ Arrays.toString(arr));
+
+
+        for (int i = 0; i < arr.length - 1; i++)
+        {
+            int index = i;
+            for (int j = i + 1; j < arr.length; j++){
+                if (arr[j] < arr[index]){
+                    index = j;//searching for lowest value
+                }
+            }
+            // swapping between the smallest number and the inde
+            int temp = arr[index];
+            arr[index] = arr[i];
+            arr[i] = temp;
+        }
+        System.out.println("after sorting: "+Arrays.toString(arr));
+
+
+    }
+
+
+    public static void main(String[] args) {
+int[] arr={4,1,8,5,7,3,6};
+selectionsorting(arr); // passing the array to tghe selectionmethod .
+    }
+}
+
+
+/*output
+* Before sorting : [4, 1, 8, 5, 7, 3, 6]
+after sorting: [1, 3, 4, 5, 6, 7, 8]
+* */
